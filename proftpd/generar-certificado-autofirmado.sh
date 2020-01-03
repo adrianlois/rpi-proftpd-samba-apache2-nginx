3e4b6fcc32ab174a59b4365b54fd7ffc94290793
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2020-01-03 15:13:53 +0100

    proftpd

--- proftpd/generar-certificado-autofirmado.sh
+++ proftpd/generar-certificado-autofirmado.sh
@@ -1,4 +1,5 @@
 # Crea directorio donde se almacerán los certificados para la conexión segurida FTP con proftpd.
 mkdir /etc/proftpd/ssl
+
 # Genera certificados con OpenSSL
 openssl req -new -x509 -days 365 -nodes -out /etc/proftpd/ssl/proftpd.cert.pem -keyout /etc/proftpd/ssl/proftpd.key.pem
